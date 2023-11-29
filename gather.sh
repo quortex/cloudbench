f57e040df9ce3234930d8a2dad111ba5874b52c6
Marc Baillavoine <mbaillavoine@synamedia.com>
2023-11-29 14:29:49 +0100

    "Use the new aws script. Note it will break GCP and Azure pricing"

--- gather.sh
+++ gather.sh
@@ -10,17 +10,30 @@ rm -f $PRICE
 
 echo ${PWD}
 
-function get_raw_price {
-    local cloud=$1
-    local machine=$2
-    local model=$3
+#function get_raw_price {
+#    local cloud=$1
+#    local machine=$2
+#    local model=$3
 
-    pushd pricing/$cloud &> /dev/null && \
-        item_price=$(./price.sh $machine $model  | grep "Tot Price" | cut -d":" -f 2 | sed "s/\,/\./g") && \
-    popd &> /dev/null
+#    pushd pricing/$cloud &> /dev/null && \
+#        item_price=$(./price.sh $machine $model  | grep "Tot Price" | cut -d":" -f 2 | sed "s/\,/\./g") && \
+#    popd &> /dev/null
 
-    echo $item_price
-}
+#    echo $item_price
+#}
+
+#function make_price_old {
+#    local cloud=$1
+#    local machine=$2
+#    local model=$3
+#    local duration=$4
+
+#    item_price=$(get_raw_price $cloud $machine $model)
+#    price=$(echo "scale=10;$item_price * $duration / (1000*3600)" | bc)
+#    echo $price
+#}
+
+
 
 function make_price {
     local cloud=$1
@@ -28,15 +41,28 @@ function make_price {
     local model=$3
     local duration=$4
 
-    item_price=$(get_raw_price $cloud $machine $model)
+    local item_price=0
+
+    #echo "Cloud: $cloud"
+    #echo "Machine: $machine"
+    #echo "Model: $model"
+    #echo "Duration: $duration"
+
+    if [ "$cloud" == "aws" ]; then
+        item_price=$(./awsprice.sh "$machine" "$model")
+    else
+        item_price=0
+    fi 
+
+    #item_price=$(get_raw_price $cloud $machine $model)
     price=$(echo "scale=10;$item_price * $duration / (1000*3600)" | bc)
     echo $price
 }
 
 filelist=$(ls $1-*.csv)
 
-echo "Cloud Provider;Machine Type;File;Video PID;Input Duration;Encoding Name;Processing Duration;On-Demand Pricing (qxo);Preemptible Pricing (qxp);1 Year Commit Price (qx1);(3 Years Commit Price (qx3)" > $OUTPUT
-echo "Cloud Provider;Machine Type;Total Duration;On-Demand Pricing (qxo);Preemptible Pricing (qxp);1 Year Commit Price (qx1);(3 Years Commit Price (qx3)" > $SUMMARY
+echo "Cloud Provider;Machine Type;File;Video PID;Input Duration;Encoding Name;Processing Duration;On-Demand Pricing (qxo);Preemptible Pricing (qxp);1 Year Commit Price (qx1);3 Years Commit Price (qx3)" > $OUTPUT
+echo "Cloud Provider;Machine Type;Total Duration;On-Demand Pricing (qxo);Preemptible Pricing (qxp);1 Year Commit Price (qx1);3 Years Commit Price (qx3)" > $SUMMARY
 echo "Cloud Provider;Machine Type;On-Demand Pricing / h;Preemptible Pricing / h;1 Year Commit Price / h;3 Years Commit Price / h" > $PRICE
 
 for file in $filelist; do
@@ -45,10 +71,10 @@ for file in $filelist; do
     cloud=$(echo $full_machine |cut -d'-' -f 1)
     echo "Processing $file ..."
 
-    ondemand_price=$(get_raw_price $cloud $machine "ondemand")
-    preemptible_price=$(get_raw_price $cloud $machine "preemptible")
-    oneyrcommit_price=$(get_raw_price $cloud $machine "1yrcommit")
-    threeyrcommit_price=$(get_raw_price $cloud $machine "3yrcommit")    
+    #ondemand_price=$(get_raw_price $cloud $machine "ondemand")
+    #preemptible_price=$(get_raw_price $cloud $machine "preemptible")
+    #oneyrcommit_price=$(get_raw_price $cloud $machine "1yrcommit")
+    #threeyrcommit_price=$(get_raw_price $cloud $machine "3yrcommit")    
 
     echo "$cloud;$full_machine;$ondemand_price;$preemptible_price;$oneyrcommit_price;$threeyrcommit_price" >> $PRICE
 
@@ -57,15 +83,15 @@ for file in $filelist; do
         duration=$(echo $line | rev | cut -d';' -f 1 | rev)
         total_duration=$(($total_duration + $duration))
         ondemand_price=$(make_price $cloud $machine "ondemand" $duration)
-        preemptible_price=$(make_price $cloud $machine "preemptible" $duration)
-        oneyrcommit_price=$(make_price $cloud $machine "1yrcommit" $duration)
-        threeyrcommit_price=$(make_price $cloud $machine "3yrcommit" $duration)
+        preemptible_price=$(make_price $cloud $machine "spot" $duration)
+        oneyrcommit_price=$(make_price $cloud $machine "1yr" $duration)
+        threeyrcommit_price=$(make_price $cloud $machine "3yr" $duration)
         echo "$cloud;$full_machine;$line;$ondemand_price;$preemptible_price;$oneyrcommit_price;$threeyrcommit_price" >> $OUTPUT
     done
     ondemand_price=$(make_price $cloud $machine "ondemand" $total_duration)
-    preemptible_price=$(make_price $cloud $machine "preemptible" $total_duration)
-    oneyrcommit_price=$(make_price $cloud $machine "1yrcommit" $total_duration)
-    threeyrcommit_price=$(make_price $cloud $machine "3yrcommit" $total_duration)
+    preemptible_price=$(make_price $cloud $machine "spot" $total_duration)
+    oneyrcommit_price=$(make_price $cloud $machine "1yr" $total_duration)
+    threeyrcommit_price=$(make_price $cloud $machine "3yr" $total_duration)
     echo "$cloud;$full_machine;$total_duration;$ondemand_price;$preemptible_price;$oneyrcommit_price;$threeyrcommit_price" >> $SUMMARY
 done
 
