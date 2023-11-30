cc2b9c3dcfce597043b0c82e3b363cbc9075671a
Marc Baillavoine <mbaillavoine@synamedia.com>
2023-11-30 07:57:40 +0100

    + Added a cache to awsprice to avoid calling the APIs all the time

--- awsprice.sh
+++ awsprice.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+mkdir -p .awsprice
+CACHE_PERIOD=86400
+
 # API_REGION is the AWS region where the API requests will be directed. It's *not* the region 
 # corresponding to where the prices belong! (we only use Ireland)
 API_REGION="us-east-1"
@@ -104,25 +107,42 @@ if [ "$LEASE_PLAN" == "" ]; then
     help
 fi
 
-echo "Will get price using profile \"$AWS_PROFILE\" for \"$INSTANCE_TYPE\" using lease plan \"$LEASE_PLAN\"" >&2
+#echo "Will get price using profile \"$AWS_PROFILE\" for \"$INSTANCE_TYPE\" using lease plan \"$LEASE_PLAN\"" >&2
 
-# Main logic to determine which pricing information to fetch based on the user input.
-# The script supports different pricing types: ondemand, 1yr, 3yr, and spot.
-case $LEASE_PLAN in
-    ondemand)
-        price=$(get_ondemand_price "$INSTANCE_TYPE")
-        ;;
-    1yr)
-        price=$(get_instancesavingplans_price "$INSTANCE_TYPE" "$LEASE_PLAN")
-        ;;
-    3yr)
-        price=$(get_instancesavingplans_price "$INSTANCE_TYPE" "$LEASE_PLAN")
-        ;;
-    spot)
-        price=$(get_spot_price "$INSTANCE_TYPE")
-        ;;
-    *)
-        price=0
-    esac
+CACHE_FILE=".awsprice/${LEASE_PLAN}-${INSTANCE_TYPE}"
+
+price=""
+
+if [ -f $CACHE_FILE ]; then
+    modified=$(ls -l -D "%s" "$CACHE_FILE" | awk '{print $6}')
+    now=$(date +%s)
+    delta=$(($now-$modified))
+    if [ $delta -lt $CACHE_PERIOD ]; then
+        #echo "Will use cached data" >&2
+        price=$(cat $CACHE_FILE)
+    fi
+fi
+
+if [ "$price" == "" ]; then
+    # Main logic to determine which pricing information to fetch based on the user input.
+    # The script supports different pricing types: ondemand, 1yr, 3yr, and spot.
+    case $LEASE_PLAN in
+        ondemand)
+            price=$(get_ondemand_price "$INSTANCE_TYPE")
+            ;;
+        1yr)
+            price=$(get_instancesavingplans_price "$INSTANCE_TYPE" "$LEASE_PLAN")
+            ;;
+        3yr)
+            price=$(get_instancesavingplans_price "$INSTANCE_TYPE" "$LEASE_PLAN")
+            ;;
+        spot)
+            price=$(get_spot_price "$INSTANCE_TYPE")
+            ;;
+        *)
+            price=0
+        esac
+        echo $price > $CACHE_FILE
+fi
 
 echo $price
